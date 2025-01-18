63f01de2ba0400539bad4d506d1b7000eb1db4b0
Jersy Baltazar <jersy.baltazar.c@gmail.com>
2025-01-18 08:42:58 -0500

    des

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -98,8 +98,8 @@ const Header: FC<Props> = ({
           <div
             className={`${
               active
-                ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
-                : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
+                ? "bg-white/100 bg-opacity-80 dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
+                : "bg-transparent w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
             }`}
           >
             <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
